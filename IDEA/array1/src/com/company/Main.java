dad8fee35c557510acae39f24ec4ec938b804ac9
Bahram Hajian <bahajian@yahoo.com>
2017-06-06 12:32:13 -0400

    Array1 commit

---
+++ IDEA/array1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hi This is new Project of array1");
+
+        // write your code here
+    }
+}
